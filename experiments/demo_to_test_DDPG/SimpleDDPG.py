8770d5482c0aec75b94be92ff342325d8f0cd77a
BurritoKitten <nhemming28@gmail.com>
2023-07-09 11:47:37 -0500

    Shells for running test simulation for DQN and DDPG added

---
+++ experiments/demo_to_test_DDPG/SimpleDDPG.py
@@ -0,0 +1,35 @@
+"""
+used for testing and debugging DDPG using a simple massless agent
+"""
+
+# native modules
+
+# 3rd party modules
+
+# own modules
+from environment.NavigationEnvironment import NavigationEnvironment
+
+
+def main():
+
+    env = NavigationEnvironment()
+    test_scenario_number = 1
+    input_file_name = ''
+    if test_scenario_number == 0:
+        input_file_name = 'experiment_setup_DDPG_vector_control.yaml'
+    elif test_scenario_number == 1:
+        input_file_name = 'experiment_setup_DDPG_bspline_control.yaml'
+    elif test_scenario_number == 2:
+        input_file_name = 'experiment_setup_DDPG_dubins_control.yaml'
+    elif test_scenario_number == 3:
+        input_file_name = 'experiment_setup_DDPG_point_control.yaml'
+
+    env.build_env_from_yaml(input_file_name)
+
+    # run the training
+    env.train_agent()
+
+
+if __name__ == '__main__':
+
+    main()
